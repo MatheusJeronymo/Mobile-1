2e3a3da65f1108c4dc25d571bfe42e25acdbfe41
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-05 22:46:38 -0300

    teste

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
-import { TextInput } from 'react-native-paper';
 import LoginScreen from './src/screen/LoginScreen';
 
 const App = () => {
   return (
-    <ImageBackground source = {require('./assets/fundo.png')}>
       <LoginScreen />
-    </ImageBackground>
     );
 };
 
--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import {
-    View,
+    Text,
     Button,
+    Image,
+    ImageBackground,
+    StyleSheet,
 } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 const LoginScreen = () => {
     return (
+        <ImageBackground style={styles.imageBackground} source = {require('../../assets/fundo.png')}>
+        <Image style={styles.imageLogo} source = {require('../../assets/logo.png')}/>
+        <Text style={styles.textTitle}>Entrar</Text>
         <TextInput
-        label="Email"
-        value="aaaaaaaaaaaa"
+            placeholder="Username"
         />
+        <TextInput
+            placeholder="Senha"
+        />
+        <Button>
+            Entrar
+        </Button>
+        </ImageBackground>
+
     );
 };
 
+const styles = StyleSheet.create({
+    imageBackground:{
+      flex:1,
+      resizeMode: 'conver',
+      justifyContent : 'center',
+      alignItems: 'center',
+      padding: 16,
+    },
+    imageLogo:{
+        width:150,
+        margin: 16,
+        resizeMode: 'contain',
+        height: 50,
+    },
+    textTitle:{
+        fontSize: 26,
+        fontWeight: 'bold',
+    },
+  });
 
 export default LoginScreen;
