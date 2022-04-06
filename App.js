5d067c298ff6e3b163ae547c939e77a091b5b1b7
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-05 22:23:56 -0300

    start

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ImageBackground } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import LoginScreen from './src/screen/LoginScreen';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <ImageBackground source = {require('./assets/fundo.png')}>
+      <LoginScreen />
+    </ImageBackground>
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App; 
---
+++ src/screen/LoginScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    View,
+    Button,
+} from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+const LoginScreen = () => {
+    return (
+        <TextInput
+        label="Email"
+        value="aaaaaaaaaaaa"
+        />
+    );
+};
+
+
+export default LoginScreen;
